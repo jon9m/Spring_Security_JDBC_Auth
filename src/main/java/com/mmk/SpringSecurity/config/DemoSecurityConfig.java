994d3be7b6931e8c14a2dfa6c4aa207702a57a95
jon <manojam79@gmail.com>
2019-03-21 19:14:40 +1100

    Login and Logout

--- src/main/java/com/mmk/SpringSecurity/config/DemoSecurityConfig.java
+++ src/main/java/com/mmk/SpringSecurity/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.mmk.SpringSecurity.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,10 +16,27 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// Add users for the in memory authentication
 		UserBuilder users = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication()
-				.withUser(users.username("manoj").password("test123").roles("ADMIN"))
+		auth.inMemoryAuthentication().withUser(users.username("manoj").password("test123").roles("ADMIN"))
 				.withUser(users.username("sam").password("test123").roles("USER"))
 				.withUser(users.username("mmk").password("test123").roles("GUSET"))
 				.withUser(users.username("jon").password("test123").roles("USER"));
 	}
+
+
+	// Custom Login Form
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/showLoginPage")
+			.loginProcessingUrl("/authenticateTheUser")
+			.permitAll()
+			.and()
+			.logout()			
+			.permitAll();
+	}
+
 }
--- src/main/webapp/WEB-INF/view/home.jsp
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
@@ -6,5 +6,10 @@
 </head>
 <body>
 	<h1>Home</h1>
+	<form:form action="${pageContext.request.contextPath}/logout" method="POST">		
+		<p>
+			<input type="submit" value="Logout">
+		</p>
+	</form:form>
 </body>
 </html>
